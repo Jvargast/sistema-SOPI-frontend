7c05dd7fa9649cfb0ea525bef8bc04a5f47361f3
Jose <51755883+Jvargast@users.noreply.github.com>
2022-11-21 15:47:20 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    screens: {
+      sm:"640px"
+      //@media(min-width: 640px) { ... }
+    }
+  },
+  plugins: [],
+}
